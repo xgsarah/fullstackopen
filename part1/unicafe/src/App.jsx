25fbc78ab459846c38b599483d2e760fd385513e
xgsarah <sjbquimpo@gmail.com>
2024-04-07 12:27:07 +0800

    refactor(part1 - unicafe): move Statistics into own component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react';
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  
+  return (
+    <div>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good - bad) / (good + bad + neutral) || 0}</div>
+      <div>positive {(good / (good + bad + neutral)) * 100 || 0} %</div>
+    </div>
+  );
+};
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -14,13 +29,7 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <h2>statistics</h2>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {good + neutral + bad}</div>
-      <div>average {(good - bad) / (good + bad + neutral) || 0}</div>
-      <div>positive {(good / (good + bad + neutral)) * 100 || 0} %</div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
